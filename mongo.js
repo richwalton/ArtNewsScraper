8f2152133123425cb8cd56658b022c6a072e603d
Rich Walton <sbtsun@yahoo.com>
2021-03-09 16:03:21 -0700

    config & server settings add procfile

--- mongo.js
+++ mongo.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb+srv://richwalton:Z7QpJbEqnRS5ib3D@cluster0.dyuho.mongodb.net/art_news_database?retryWrites=true&w=majority';
+// const MONGODB_URI = 'mongodb+srv://richwalton:Z7QpJbEqnRS5ib3D@cluster0.dyuho.mongodb.net/art_news_database?retryWrites=true&w=majority';
+require('dotenv').config()
 const port = process.env.PORT || 3000;
 app.listen(port);
 
 module.exports = async () => {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
