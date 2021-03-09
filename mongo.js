3f57943ce8b08024d5acdea0cab983ef16ba8b75
Rich Walton <sbtsun@yahoo.com>
2021-03-08 23:01:02 -0700

    added mongo uri

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const mongoArtData = 'mongodb+srv://richwalton:Z7QpJbEqnRS5ib3D@cluster0.dyuho.mongodb.net/art_news_database?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://richwalton:Z7QpJbEqnRS5ib3D@cluster0.dyuho.mongodb.net/art_news_database?retryWrites=true&w=majority'
 
 module.exports = async () => {
-    await mongoose.connect(mongoArtData, {
+    await mongoose.connect(MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
