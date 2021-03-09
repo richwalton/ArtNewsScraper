f0c4323909ea466f173532da7474841155fabd77
Rich Walton <sbtsun@yahoo.com>
2021-03-09 15:31:04 -0700

    remove procfile

--- mongo.js
+++ mongo.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb+srv://richwalton:Z7QpJbEqnRS5ib3D@cluster0.dyuho.mongodb.net/art_news_database?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://richwalton:Z7QpJbEqnRS5ib3D@cluster0.dyuho.mongodb.net/art_news_database?retryWrites=true&w=majority';
+const port = process.env.PORT || 3000;
+app.listen(port);
 
 module.exports = async () => {
     await mongoose.connect(MONGODB_URI, {
