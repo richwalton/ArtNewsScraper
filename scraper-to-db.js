088c77515357200d9a2c9dd6e697d6a5684b079b
Rich Walton <sbtsun@yahoo.com>
2021-03-08 22:02:15 -0700

    change cron

--- scraper-to-db.js
+++ scraper-to-db.js
@@ -139,7 +139,7 @@ function scrape () {
         }
     })
 }
-cron.schedule('0 */8 * * * ', () => {     // <----- cron - run 3 times a day **** ###
+cron.schedule('4 * * * * ', () => {     // '0 */8 * * * ' <----- cron - run 3 times a day **** ###
 scrape().then(function(value) { 
     let artColArray = value; 
     let artNewsPa = artColArray.artNP;
